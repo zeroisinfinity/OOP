b6d47a066de2da089933d03129ed4c05d7e958c7
Sahil More <skm842011@gmail.com>
2024-09-27 16:43:51 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,25 +6,30 @@ template <typename Gen>
 void selection_sort(Gen arr[] , int records){//b1
 
         
-        Gen current_min; 
-        
-        for(int iter = 0 ; iter < records ; iter++){//b2
-        
-            for(int cf = 1 ; cf < records ; cf++){//b3
+        Gen temp; 
+        int min;
+        for(int iter = 0 ; iter < records - 1 ; iter++){//b2
+            min = iter;
+            for(int cf = iter + 1 ; cf < records ; cf++){//b3
             
-                if(arr[iter] > arr[cf]){//b4
-                    current_min = arr[cf];
-                    arr[cf] = arr[iter];
-                    arr[iter] = current_min;
+                if(arr[min] > arr[cf]){//b4
+                    min = cf;
+                   
                    }//b4
                    
-            }//b3
-            
+           }//b3
+                if(min!=iter){//b6
+                    temp = arr[iter];
+                    arr[iter] = arr[min];
+                    arr[min] = temp;
+                    }//b6
+                 
          }//b2
          
          for(int show = 0 ; show  < records ; show++){//b5
                 cout<<arr[show]<<" ";
          }//b5
+         cout<<endl;
          
 }//b1
 
@@ -32,8 +37,9 @@ int main(){//b1
            
          int choice;
          int size;
-         float inp_arr_fl[size];
-         int inp_arr_it[size];
+         float* inp_arr_fl = new float[size];
+         int* inp_arr_it = new int[size];
+         int assign;
          
          do{//b2
            
@@ -53,30 +59,34 @@ int main(){//b1
                     
                      cout<<"Enter no.of records in Float Array - ";
                      cin>>size; 
+
                      cout<<"Enter the elements into array"<<endl;
-                     for(int assign = 0 ; assign < size ; assign++){//b4
+                     for(assign = 0 ; assign < size ; assign++){//b4
                             cout<<"Enter the "<<assign+1<<" number element -----> ";
                             cin>>inp_arr_fl[assign];
                             cout<<endl;
                          }//b4
-                     selection_sort<float>(inp_arr_fl,size);
+                     selection_sort(inp_arr_fl,size);
+                     delete[] inp_arr_fl;
                      break;
                      
                 case 2:
                     
                      cout<<"Enter no.of records in Integer Array - ";
                      cin>>size; 
+                   
                      cout<<"Enter the elements into array"<<endl;
-                     for(int assign = 0 ; assign < size ; assign++){//b5
+                     for(assign = 0 ; assign < size ; assign++){//b5
                             cout<<"Enter the "<<assign+1<<" number element -----> ";
                             cin>>inp_arr_it[assign];
                             cout<<endl;
                          }//b5
-                     selection_sort<int>(inp_arr_it,size);
+                     selection_sort(inp_arr_it,size);
+                     delete[] inp_arr_it;
                      break;
                 default:
                      cout<<"Thank You!! For using our Program"<<endl;
-                     exit(0);
+                     return 0;
               
                    }//b3
             }//b2
